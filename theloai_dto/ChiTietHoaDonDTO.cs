1f3016bb4709d19f604b57fad93ed3d0c1293b72
vanhieu123555 <geghedsgg@gmail.com>
2025-06-15 23:16:53 +0700

    Add files via upload

---
+++ theloai_dto/ChiTietHoaDonDTO.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace theloai_dto
+{
+    public class ChiTietHoaDonDTO
+    {
+        public int MaChiTiet { get; set; }
+        public int MaHoaDon { get; set; }
+        public int MaSach { get; set; }
+        public int SoLuong { get; set; }
+        public decimal DonGia { get; set; }
+        public string TenSach { get; set; } // Thêm thuộc tính TenSach
+
+        public ChiTietHoaDonDTO(int maChiTiet, int maHoaDon, int maSach, int soLuong, decimal donGia)
+        {
+            MaChiTiet = maChiTiet;
+            MaHoaDon = maHoaDon;
+            MaSach = maSach;
+            SoLuong = soLuong;
+            DonGia = donGia;
+            TenSach = TenSach;
+        }
+
+        public ChiTietHoaDonDTO() { } // Constructor mặc định
+    }
+}
